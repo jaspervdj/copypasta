eb84d337af1b0f3d741204a196d256e7473dad47
Toon Willems <willemstoon@gmail.com>
2010-02-02 10:47:46 +0100

    fixed stupid bugs

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -8,23 +8,25 @@ class PrimeGenerator {
 	/*
 	generate all prime numbers upto (inclusive) n
 	*/
-	public static ArrayList<Integer> generate(int n){
-		boolean[] sieve = new boolean[n];
+	public static ArrayList<Integer> generate(int n)
+	{
+		boolean[] sieve = new boolean[n+1];
 		double sqrt = Math.sqrt(n);
 		for(int i = 2;i<=sqrt; i++){
-			for(int j= 2; i*j < n;j++){
-				boolean[i*j]=true;
+			for(int j= 2; i*j <= n;j++){
+				sieve[i*j]=true;
 			}
 		}
 		
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		for (int i=0; i<n; i++) {
-			if(! sieve[i]) list.append(sieve[i]+1)
+		for (int i=0; i<=n; i++) {
+			if(! sieve[i]) list.add(i);
 		}
 		return list;
 	}
 	
-	public static void main(String[] args) {
-		Test.test
+	public static void main(String[] args)
+	{
+		
 	}
 }
